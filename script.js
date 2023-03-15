e000427363e1246c8701950ef1ede0875833d575
Haifa-saud <105865534+Haifa-saud@users.noreply.github.com>
2023-03-16 00:53:56 +0300

    Link with openAI done

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("submit-btn").addEventListener("click", function () {
+    sendToChatGPT();
+});
+
+// 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
+
+function sendToChatGPT() {
+    let value = document.getElementById("word-input").value;
+
+    let body = {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: value }],
+        temperature: 1,
+    };
+
+    let headers = {
+        Authorization: "Bearer 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",
+    };
+
+    axios
+        .post("https://api.openai.com/v1/chat/completions", body, {
+            headers: headers,
+        })
+        .then((response) => {
+            let reply = response.data.choices[0].message.content;
+            document.getElementById("reply-content").textContent = reply;
+        });
+}
