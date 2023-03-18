04d53cdd2984cdeb9f44d4fb105826039dc253b8
Haifa-saud <105865534+Haifa-saud@users.noreply.github.com>
2023-03-18 15:44:30 +0300

    app commit

---
+++ build/precache-manifest.02df395e8f2257be0f7d0d3b91d390fc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "678064020f1033082bd6",
+    "url": "/ReadaTeck/static/css/main.810f3b9c.chunk.css"
+  },
+  {
+    "revision": "678064020f1033082bd6",
+    "url": "/ReadaTeck/static/js/main.67806402.chunk.js"
+  },
+  {
+    "revision": "a89266deb73d87b01859",
+    "url": "/ReadaTeck/static/js/1.a89266de.chunk.js"
+  },
+  {
+    "revision": "ce573d86c87e42c4803e",
+    "url": "/ReadaTeck/static/css/2.5b56a39f.chunk.css"
+  },
+  {
+    "revision": "ce573d86c87e42c4803e",
+    "url": "/ReadaTeck/static/js/2.ce573d86.chunk.js"
+  },
+  {
+    "revision": "71505016b629b8739a54",
+    "url": "/ReadaTeck/static/js/runtime~main.71505016.js"
+  },
+  {
+    "revision": "c9d4f742165345874e3bf9a55843ca0b",
+    "url": "/ReadaTeck/static/media/bottomImage.c9d4f742.jpeg"
+  },
+  {
+    "revision": "a189c5844c0af95c17905c5c197003bb",
+    "url": "/ReadaTeck/static/media/chatGPTbackground.a189c584.jpeg"
+  },
+  {
+    "revision": "5eb9b7a87d77c02c40077304bbb49b60",
+    "url": "/ReadaTeck/index.html"
+  }
+];
